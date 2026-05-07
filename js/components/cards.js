8cc95939570752b6b1519101df02a0588b31e4bc
miapraesttun <mia.praesttun@gmail.com>
2026-05-07 10:19:09 +0200

    funksjon for fler kort

--- js/components/cards.js
+++ js/components/cards.js
@@ -16,3 +16,22 @@ export function PrefrenceCard({title, description, icon}) {
         </div>
     `;
 }
+
+
+export function GridCard({ title, icon }) {
+    return `
+        <div class="col-6 mb-3">
+        <div class="card border-0 shadow-sm text-center grid-card h-100">
+            <div class="card-body d-flex flex-column justify-content-center align-items-center">
+
+            <div class="grid-icon mb-2">
+                <i class="bi ${icon}"></i>
+            </div>
+
+            <p class="mb-0 small fw-semibold">${title}</p>
+
+            </div>
+        </div>
+        </div>
+    `;
+}
