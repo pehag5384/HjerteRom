ba7a73e6fb7737d44706d3b35db917fac0697820
miapraesttun <mia.praesttun@gmail.com>
2026-05-11 10:32:07 +0200

    lagt inn footer

---
+++ js/components/footer.js
@@ -0,0 +1,15 @@
+
+
+export function Footer() {
+    return `
+        <nav class="footer-nav">
+
+            <i class="bi bi-map"></i>
+            <i class="bi bi-heart"></i>
+            <i class="bi bi-plus-circle"></i>
+            <i class="bi bi-search"></i>
+            <i class="bi bi-person-fill"></i>
+
+        </nav>
+    `;
+}
---
+++ js/components/header.js
@@ -0,0 +1,23 @@
+
+
+export function Header() {
+    return `
+        <header class="app-header">
+
+            <div class="d-flex justify-content-between align-items-center">
+
+                <div class="d-flex align-items-center">
+                    <i class="bi bi-geo-alt me-2"></i>
+                    <span class="fw-semibold">HR</span>
+                </div>
+
+                <div>
+                    <i class="bi bi-person-fill me-3"></i>
+                    <i class="bi bi-list"></i>
+                </div>
+
+            </div>
+
+        </header>
+    `;
+}
